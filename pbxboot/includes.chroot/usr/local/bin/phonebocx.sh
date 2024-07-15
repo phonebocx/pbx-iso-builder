1e752cf113d162e3b9c89f34ae36fe273241ed6c
Rob Thomas <xrobau@gmail.com>
2024-07-15 12:56:54 +1000

    Show the IP address of the VM

--- pbxboot/includes.chroot/usr/local/bin/phonebocx.sh
+++ pbxboot/includes.chroot/usr/local/bin/phonebocx.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+IP=$(ip -o addr show dev eth0 | awk '/inet / { print $4 }')
 # Devmode check - are we in au? This needs to be less hardcoded 8)
-if ip -o addr | grep -q 10.46; then
+if echo $IP | grep -q 10.46; then
     mkdir -p /pbxdev
     grep -q pbxdev /proc/mounts || mount repo.phonebo.cx:/livebuild/packages /pbxdev
 fi
 
 THISTTY=$(tty | sed 's!/dev/!!')
+
+echo "eth0 IP Address $IP"
 
 DIRS="/factory/core /pbxdev/core /pbx/core"
 for d in $DIRS; do
