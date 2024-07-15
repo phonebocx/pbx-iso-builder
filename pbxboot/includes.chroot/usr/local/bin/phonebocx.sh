9ba23e943718857b03f7587129a84f8ad6bbb719
Rob Thomas <xrobau@gmail.com>
2024-07-15 12:39:47 +1000

    Add 'make isotest' to speed up devel

--- pbxboot/includes.chroot/usr/local/bin/phonebocx.sh
+++ pbxboot/includes.chroot/usr/local/bin/phonebocx.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-echo "This is /usr/local/bin/phonebocx.sh - Root console on tty2"
+# Devmode check - are we in au? This needs to be less hardcoded 8)
+if ip -o addr | grep -q 10.46; then
+    mkdir -p /pbxdev
+    grep -q pbxdev /proc/mounts || mount repo.phonebo.cx:/livebuild/packages /pbxdev
+fi
+
+THISTTY=$(tty | sed 's!/dev/!!')
+
+DIRS="/factory/core /pbxdev/core /pbx/core"
+for d in $DIRS; do
+    tfile=$d/$THISTTY.sh
+    if [ -x $tfile ]; then
+        echo "I think $tfile is good"
+        exec $tfile
+        echo "How am I here?"
+        exit 9
+    fi
+done
+
+echo "/usr/local/bin/phonebocx.sh running on $THISTTY"
+echo "There is no bootfile for this tty"
 sleep infinity
 
