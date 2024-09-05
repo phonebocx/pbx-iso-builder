2f6c817a18b0262f4bac6047b6a238e75e7eb8b6
Rob Thomas <xrobau@gmail.com>
2024-09-05 12:39:31 +1000

    Make grub flexible

--- build-live-iso.sh
+++ build-live-iso.sh
@@ -22,7 +22,6 @@ rm -rf config/ *-amd64.*
 
 NAL=noautologin
 NAL=
-
 lb config \
 	--architectures amd64 --distribution ${DISTRO} --iso-application phonebocx --iso-publisher xrobau --iso-volume phonebocx \
 	--archive-areas "main contrib non-free $F" --updates true --security true --backports true \
@@ -45,16 +44,18 @@ for x in /usr/share/live/build/bootloaders/*; do
 	rsync -a $x config/bootloaders
 done
 
-# Changes to default bootloader configs are in the liveconf directory.
+# Changes to default bootloader configs are in the livebuild directory.
 # They are:
 #  syslinux_common/live.cfg.in main menu
 #  syslinux_common/menu.cfg Removed ^G beep
-#  grub-pc/grub.cfg Adds set timeout=5
+#  grub-pc/grub.cfg Rewritten
 #  grub-pc/config.cfg changes gfxmode=auto to gfxmode=1024x768x32 and disables the beep
 
 # Put our large (1024x768) live splash in place and make sure there's nothing that would
 # clobber it
 cp ${LIVESPLASHLARGEPNG} config/bootloaders/syslinux_common/splash.png
+cp ${LIVESPLASHLARGEPNG} config/bootloaders/syslinux_common/splash1024x768.png
+cp ${LIVESPLASHPNG} config/bootloaders/syslinux_common/splash800x600.png
 rm -f config/bootloaders/*/*.svg
 
 # Rsync everything in LIVEBUILDSRC over the top of config. This is checked into
@@ -64,6 +65,13 @@ rsync -a ${LIVEBUILDSRC}/ config/
 # Then merge everything from staging over the top of that. This allows
 # staging to change things in the default LIVEBUILDROOT
 rsync -a ${STAGING}/ config/
+
+# Replace __GRUBNAME__ with $THEME
+if [ ! "$GRUBNAME" ]; then
+	# Uppercase first char of Theme
+	GRUBNAME=${THEME^}
+fi
+sed -i "s/__GRUBNAME__/$GRUBNAME/g" config/bootloaders/*/*cfg
 
 # Put all our debs in place. This isn't done as part of creating STAGING because
 # I didn't want to have to care about ordering everything.
