125426bdfe346330d7e0b59af942e09d10110d0b
Rob Thomas <xrobau@gmail.com>
2025-04-03 08:48:25 +1000

    Add basic auto-nfs-mount when testing

--- livebuild/includes.chroot/usr/local/bin/phonebocx-boot.sh
+++ livebuild/includes.chroot/usr/local/bin/phonebocx-boot.sh
@@ -4,6 +4,27 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
+# If we're in a test instance, /sys/class/dmi/id/board_vendor may contain
+# 'nfs', which means we should mount whatever is there as /pbxdev
+dmibase=/sys/class/dmi/id
+if [ -e $dmibase/board_vendor ]; then
+    if grep -q nfs $dmibase/board_vendor; then
+        nfsmount=$(cat $dmibase/board_name):$(cat $dmibase/board_asset_tag)
+        pbxdev=$(grep ' /pbxdev nfs' /proc/mounts)
+        mkdir -p /pbxdev
+        if [ ! "$pbxdev" ]; then
+            # Nothing is mounted there, we're ok to mount now.
+            mount $nfsmount /pbxdev
+        else
+            # Something is mounted there, is it what should be there?
+            if ! grep -q "^$nfsmount /pbxdev nfs" /proc/mounts; then
+                umount -f /pbxdev
+                mount $nfsmount /pbxdev
+            fi
+        fi
+    fi
+fi
+
 . /usr/local/bin/phonebocx-init.sh
 
 echo $(date)": phonebocx-boot launched" >/dev/kmsg
